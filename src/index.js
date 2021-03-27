a04b5427da7d12f3f72445a9ebda4db20ba0704c
sriram10 <msriram1010@gmail.com>
2021-03-27 15:48:07 +0530

    updated routing and helmet

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import pages from './pages/FaqPage'
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
